de5c0f85dc383d057876b43b2df04f12b3c89fe4
newu2012 <newu2011@gmail.com>
2020-01-26 15:45:23 +0500

    ver 0.03
    Day 2 first patch
    Added different Idle animations to Player character so that he continues to look towards the last movement direction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,26 +5,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 2f;
 
     public Rigidbody2D rb;
     public Animator animator;
     
     private Vector2 movement;
-
+    private Vector2 lastDir;
+    
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
-        movement.Normalize();
 
-        if (movement != Vector2.zero)
+        if (movement.magnitude > 0)
         {
-            animator.SetFloat("Horizontal", movement.x);
-            animator.SetFloat("Vertical", movement.y);
-            animator.SetFloat("Speed", movement.sqrMagnitude);
+            lastDir = movement;
         }
+        
+        movement.Normalize();
+        
+        animator.SetFloat("LastHorizontal", lastDir.x);
+        animator.SetFloat("LastVertical", lastDir.y);
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
