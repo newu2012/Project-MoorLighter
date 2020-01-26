0d5f7bc72e6f9c2aedcd824982bf933474b4fe0a
newu2012 <newu2011@gmail.com>
2020-01-26 16:25:47 +0500

    ver 0.03b
    Day 2, Patch 2
    Added following ability to camera to follow Player movement
    Added "foreground" and "misc" empty tilemaps

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+
+    private void FixedUpdate()
+    {
+        var position = player.position;
+        transform.position = new Vector3(position.x, position.y, position.z - 10);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 2f;
+    private const float moveSpeed = 1.5f;
 
     public Rigidbody2D rb;
     public Animator animator;
@@ -13,28 +13,31 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 movement;
     private Vector2 lastDir;
     
+    
+    private static readonly int LastHorizontal = Animator.StringToHash("LastHorizontal");
+    private static readonly int LastVertical = Animator.StringToHash("LastVertical");
+    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int Vertical = Animator.StringToHash("Vertical");
+    private static readonly int Speed = Animator.StringToHash("Speed");
+
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
-
-        if (movement.magnitude > 0)
-        {
+        if (movement.magnitude > 0) //Если движение ещё есть, то обновляем последнее направление для стояния
             lastDir = movement;
-        }
-        
         movement.Normalize();
         
-        animator.SetFloat("LastHorizontal", lastDir.x);
-        animator.SetFloat("LastVertical", lastDir.y);
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        animator.SetFloat(LastHorizontal, lastDir.x);
+        animator.SetFloat(LastVertical, lastDir.y);
+        animator.SetFloat(Horizontal, movement.x);
+        animator.SetFloat(Vertical, movement.y);
+        animator.SetFloat(Speed, movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime * movement);
     }
 }
