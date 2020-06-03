88184036b22e461e8f891882afbc623057d20426
Kosemnik <kosizaa@mail.ru>
2020-06-03 14:40:48 +0500

    Диалоговая система
    Диалог с тестовой кнопкой для включения. Потом нужно набросить скрипты на все объекты с которыми должен быть диалог

---
+++ Assets/DialogSystem/Dialog.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Phrase
+{
+    public string SpeakerName;
+    public string Message;
+
+    public Phrase(string speacerName, string message)
+    {
+        SpeakerName = speacerName;
+        Message = message;
+    }
+}
+
+public class Dialog : MonoBehaviour
+{
+    public List<string> SpeakerNameInOrder;
+    public List<string> PhrasesInOrder;
+    public List<Phrase> Phrases;
+    public GameObject Canvas;
+
+    public void StartDialog()
+    {
+        Phrases = new List<Phrase>();
+        for (var i = 0; i < SpeakerNameInOrder.Count; i++)
+            Phrases.Add(new Phrase(SpeakerNameInOrder[i], PhrasesInOrder[i]));
+
+        Canvas.GetComponent<PlayDialog>().StartDialog(Phrases);
+    }
+}
---
+++ Assets/DialogSystem/PlayDialog.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayDialog : MonoBehaviour
+{
+    public Text Name;
+    public Text Message;
+    List<Phrase> Phrases;
+    public Canvas Canvas;
+    private int currentMessageNumber;
+
+    public void Start()
+    {
+        Canvas.enabled = false;
+    }
+
+    public void StartDialog(List<Phrase> phrases)
+    {     
+        Phrases = phrases;
+        Canvas.enabled = true;        
+        currentMessageNumber = 0;
+        ShowMessage();
+    }
+
+    public void ShowMessage()
+    {
+        Phrase currentPhrase;
+
+        if (Phrases.Count > currentMessageNumber)
+            currentPhrase = Phrases[currentMessageNumber];
+        else
+        {
+            Canvas.enabled = false;
+            return;
+        }
+
+        Name.text = currentPhrase.SpeakerName;
+        Message.text = currentPhrase.Message;        
+    }
+    
+    public void NextMessage()
+    {
+        currentMessageNumber++;
+        ShowMessage();
+    }
+}
